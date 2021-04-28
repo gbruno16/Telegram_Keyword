1d353010135d92a621f955f8a95d74ebb94b7d14
Giuseppe Bruno <gius.bruno@yahoo.it>
2021-04-28 23:58:58 +0200

    Added bot to provide push notifications when a message is forwarded to the channel

--- script.py
+++ script.py
@@ -1,19 +1,44 @@
+import os
 from telethon import TelegramClient, events
 import time
 import math
 import re 
+from telegram.ext import Updater
+import logging
 
-t_api_id = 3882522
-t_api_hash = 'b4ba5e7ea1e710aa74c9b2fb1c29ea98'
+t_api_id= os.environ['t_api_id']
+t_api_hash= os.environ['t_api_hash']
+channel_id=os.environ['channel_id']
+channel_link=os.environ['channel_link']
+
+tok=os.environ['token']
+
+updater = Updater(token=tok, use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update, context):
+    
+    chat=update.effective_chat.id
+    context.bot.send_message(chat_id=update.effective_chat.id, text=chat)                  
+    
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
 t_client = TelegramClient('anon', t_api_id, t_api_hash)
 
 @t_client.on(events.NewMessage)
 async def my_event_handler(event):
-        match=re.search('[#$]([a-zA-Z]*)',event.raw_text)
+        match=re.search('##',event.raw_text)
         if match:
-             coin=match.group(1)
-             await client.forward_messages("-1001483829428",event.message)
-)
+            
+             updater.bot.send_message(chat_id=int(channel_id),text="Canale")
+             channel= await t_client.get_entity(channel_link)
+             await t_client.forward_messages(channel,event.message)
+
 
 t_client.start()
 t_client.run_until_disconnected()
